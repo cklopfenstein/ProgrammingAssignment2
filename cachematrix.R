c23228f1921008603dbd70ed01106ae0db800162
Chris Klopfenstein <klopfenstein.chris@gmail.com>
2014-06-20 16:52:44 -0700

    Clean up and add comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,13 @@
 ## cachematrix.R
 ## Functions to calculate the inverse of a matrix, and 
 ## cache the inverse matrix. Note that an error occurs if
-## the matrix is not invertible, and the error is not 
-## handled.
+## the matrix is not invertible.
 
-## makeCacheMatrix: A function to create a "vector" of functions,
-## which take a matrix x as input, 
-## and create functions to (1) set the value of x,
+## makeCacheMatrix: A function to create a "vector" of functions.
+## This function takes a matrix x as input, 
+## and creates functions to (1) set the value of x,
 ## (2) get the value of x, (3) set (calculate) the inverse of x,
 ## and (4) get the inverse of x.
-
 
 makeCacheMatrix <- function(x = matrix()) {
     x_inverse <- NULL
@@ -23,24 +21,21 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get,
          set_inverse = set_inverse,
          get_inverse = get_inverse)
-
 }
 
-
-## cacheSolve: A function to take the inverse of a matrix.
+## cacheSolve: A function to take the inverse of a matrix. The
+## function takes a vector of functions as input
 ## If the inverse has already been calculated and cached,
 ## return the cached value.
 
-cacheSolve <- function(x, ...) {    ## input x is a vector of functions
-        ## Return a matrix that is the inverse of x$get()
-        x_inverse <- x$get_inverse()
-        if(!is.null(x_inverse)) {
+cacheSolve <- function(vec, ...) {
+        inverse <- vec$get_inverse()
+        if(!is.null(inverse)) {
 			message("getting cached data")
-			return(x_inverse)
+			return(inverse)         ## return cached inverse
 		}
-		data <- x$get()
-		x_inverse <- solve(data, ...)
-		x$set_inverse(x_inverse)
-		x_inverse
-			
+		data <- vec$get()
+		inverse <- solve(data, ...)
+		vec$set_inverse(inverse)
+		inverse                     ## Return inverse of vec$get()
 }
--- testMatrix.R
+++ testMatrix.R
@@ -1,4 +1,6 @@
 ## testMatrix.R
+setwd("~/Devel/R/DataScience/ProgrammingAssignment2")
+source("cachematrix.R")
 
 mx <- array(c((0:1), (1:0)), dim=c(2,2));
 ##mx <- matrix(c((0:1), (1:0)), nrow = 2, ncol = 2);
@@ -41,3 +43,20 @@ inv1;
 
 inv2 <- cacheSolve(mfun);
 inv2;
+
+## input a vector - set(), get() ok, error inverting. check.
+v1 <- c(1,0,0,-1);
+mfun$set(lst);
+mfun$get();
+
+## different matrix
+m4 <- matrix(c(c(1,2,3,4), c(4,3,2,1), c(1,-2,-3,4), c(-4,3,2,-1)),
+        nrow = 4, ncol = 4);
+mfun$set(m4);
+mfun$get();
+m4inv <- cacheSolve(mfun);
+m4inv;
+m4inv <- cacheSolve(mfun);
+m4inv;
+id <- m4 %*% m4inv;
+id;
